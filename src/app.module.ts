f3b51595d186dc71cdcb4520c6d35443dd2dff47
ChenJunff <270757002@qq.com>
2022-01-28 16:04:09 +0800

    feat: 初始化moongodb连接,初始化增删改查

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://47.98.149.5/nest', {
+      // MongooseModule.forRoot('mongodb://nestUser:password@47.98.149.5/nest', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    BlogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
