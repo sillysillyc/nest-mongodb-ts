5513324329fb083d8d32bf6b42bbe86d47343337
ChenJunff <270757002@qq.com>
2022-02-17 11:10:27 +0800

    feat: 新增一个错误过滤器、响应数据格式统一处理器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(3000, () => {
+    Logger.log('app start at: http://localhost:3000');
+  });
 }
 bootstrap();
