ea098ab5ecb13079986a350f5ecc965594a1ca8a
ChenJunff <270757002@qq.com>
2022-09-30 10:49:08 +0800

    feat: specific collection name

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -5,7 +5,11 @@ import { BlogController } from './blog.controller';
 import { BlogSchema } from '../schemas';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'blog', schema: BlogSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'blog', schema: BlogSchema, collection: 'blog' },
+    ]),
+  ],
   providers: [BlogService],
   controllers: [BlogController],
 })
