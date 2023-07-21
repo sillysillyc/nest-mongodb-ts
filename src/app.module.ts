010e6636c49c62a3bb8d1ecf0d20313ac966b6c9
陈俊帆 <chenjunfan1@cmiot.chinamobile.com>
2023-07-21 11:06:52 +0800

    feat: AppModule 根据环境选择不同环境变量文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
-import * as dotenv from 'dotenv';
+import { config } from 'dotenv';
 
-dotenv.config({ path: '.env.local' });
-
+config({
+  path: process.env.NODE_ENV === 'development' ? '.env.local' : '.env',
+});
 const { DB_USER, DB_PWD, DB_ADDRESS, DB_NAME, DB_PORT } = process.env;
 export const dbLink = `mongodb://${DB_USER}:${encodeURIComponent(
   DB_PWD,
